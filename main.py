52d6b841824ef2a7ae91c7b6e1e75aff4c562ce2
Константин Шульженко <konstantinsulzenko@MacBook-Pro-Konstantin.local>
2023-01-14 15:09:31 +0300

    change main

--- main.py
+++ main.py
@@ -44,24 +44,55 @@ def format_tel(contacts):
 
 def duplicates(contacts):
     new_contacts_list = []
+    new_dict_1= {}
+    new_dict_2 = {}
 
-    for get_fi in contacts:
-        for get_fi_com in contacts:
-            if get_fi[0] == get_fi_com[0] and get_fi[1] == get_fi_com[1]:
-                if get_fi[2] == '':
-                    get_fi[2] = get_fi_com[2]
-                if get_fi[3] == '':
-                    get_fi[3] = get_fi_com[3]
-                if get_fi[4] == '':
-                    get_fi[4] = get_fi_com[4]
-                if get_fi[5] == '':
-                    get_fi[5] = get_fi_com[5]
-                if get_fi[6] == '':
-                    get_fi[6] = get_fi_com[6]
+    for rt in contacts [1:]:
+        if rt[0] not in new_dict_1.keys():
+            new_dict_1[rt[0]] = rt[1:]
+        elif rt[0] in new_dict_1.keys():
+            new_dict_2[rt[0]] = rt[1:]
 
-    for new_list in contacts:
-        if new_list not in new_contacts_list and len(new_list) < 8:
-            new_contacts_list.append(new_list)
+    for key, value in new_dict_1.items():
+        for key2, value2 in new_dict_2.items():
+            if key == key2:
+                if value[0] == '':
+                    value[0] = value2[0]
+                if value[1] == '':
+                    value[1] = value2[1]
+                if value[2] == '':
+                    value[2] = value2[2]
+                if value[3] == '':
+                    value[3] = value2[3]
+                if value[4] == '':
+                    value[4] = value2[4]
+                if value[5] == '':
+                    value[5] = value2[5]
+        value_3 = value
+        value_3.insert(0,key)
+        new_contacts_list.append(value_3)
+
+
+    new_contacts_list.insert(0,contacts[0])
+    # print(new_contacts_list)
+
+    # for get_fi in contacts:
+    #     for get_fi_com in contacts:
+    #         if get_fi[0] == get_fi_com[0] and get_fi[1] == get_fi_com[1]:
+    #             if get_fi[2] == '':
+    #                 get_fi[2] = get_fi_com[2]
+    #             if get_fi[3] == '':
+    #                 get_fi[3] = get_fi_com[3]
+    #             if get_fi[4] == '':
+    #                 get_fi[4] = get_fi_com[4]
+    #             if get_fi[5] == '':
+    #                 get_fi[5] = get_fi_com[5]
+    #             if get_fi[6] == '':
+    #                 get_fi[6] = get_fi_com[6]
+    #
+    # for new_list in contacts:
+    #     if new_list not in new_contacts_list and len(new_list) < 8:
+    #         new_contacts_list.append(new_list)
 
     write_file(new_contacts_list)
 
